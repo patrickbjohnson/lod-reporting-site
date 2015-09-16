b17f56ca42ba4bddbdeba61cf810dd01b4e69350
Patrick Johnson <im@pbj.me>
2015-09-16 01:32:14 -0400

    add asset directories

---
+++ wp-content/themes/life-or-death-client-reporting/assets/js/built/main.bundle.js
@@ -0,0 +1,9 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports) {
+
+	console.log('Hello from life-or-death-client-reporting');
+
+/***/ }
+]);
+//# sourceMappingURL=main.bundle.js.map
---
+++ wp-content/themes/life-or-death-client-reporting/assets/js/src/main.js
@@ -0,0 +1,1 @@
+console.log('Hello from life-or-death-client-reporting');
