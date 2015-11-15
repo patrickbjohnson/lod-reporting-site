5ae67444886a87f515467fa59c06611bc94e3b47
Patrick Johnson <im@pbj.me>
2015-11-14 23:07:26 -0500

    update footer

--- wp-content/themes/life-or-death-client-reporting/footer.php
+++ wp-content/themes/life-or-death-client-reporting/footer.php
@@ -1,7 +1,15 @@
     <footer class="footer">
         <div class="page-container">
-            <a href="<?php echo admin_url( '/profile.php' ); ?>">View Profile</a>
-            <a class="site-company" href="http://wegetpress.com/" target="_blank">Life or Death PR</a>
+        	<div class="footer__group">
+        		<a class="footer__logo footer__group-item">
+        			<svg role="img" title="CodePen" viewBox="0 0 184 129">
+        			    <use xlink:href="<?php BPAssetHelper::the_image('svgsprite.svg');?>#lod-logo"/>
+        			</svg>
+        		</a>
+        	</div>
+        	<div class="footer__group">
+	    		<a href="<?php echo admin_url( '/profile.php' ); ?>">View Profile</a>	
+        	</div>
         </div>
         <a class="site-credit" href="http://pbj.me" target="_blank">Built By pbj</a>
     </footer>
